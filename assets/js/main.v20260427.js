a8ef235ba138f6bd5c250891ceedd5c25cffaac1
Justine Paul Mundackal <justinepaulmundackal@gmail.com>
2026-06-26 10:44:32 +0200

    Add ChefCoco and ConsultFox pages and simplify homepage tile styling

--- assets/js/main.v20260427.js
+++ assets/js/main.v20260427.js
@@ -127,16 +127,19 @@
 				x;
 
 			// Image.
+				if ($this.data('no-bg-image') !== true && $img.length > 0) {
 
-				// Set image.
-					$this.css('background-image', 'url(' + $img.attr('src') + ')');
+					// Set image.
+						$this.css('background-image', 'url(' + $img.attr('src') + ')');
 
-				// Set position.
-					if (x = $img.data('position'))
-						$image.css('background-position', x);
+					// Set position.
+						if (x = $img.data('position'))
+							$image.css('background-position', x);
 
-				// Hide original.
-					$image.hide();
+					// Hide original.
+						$image.hide();
+
+				}
 
 			// Link.
 				if ($link.length > 0) {
